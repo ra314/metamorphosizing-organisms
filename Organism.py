5eadde0f2c2b6f8e1a4dafb12c4a3bdf91c7bc6d
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-04-15 22:39:55 +1000

    Changed variable names in Oragnism.

--- Organism.py
+++ Organism.py
@@ -1,7 +1,7 @@
 class Organism:
-	def __init__(self, name, ability, tile_type, num_tiles_to_activate):
+	def __init__(self, name, ability, mana_type, num_mana_to_activate):
 		self.name = name
 		self.ability = ability
-		self.tile_type = tile_type
-		self.num_tiles = 0
-		self.num_tiles_to_activate = num_tiles_to_activate
+		self.mana_type = mana_type
+		self.num_mana = 0
+		self.num_mana_to_activate = num_mana_to_activate
