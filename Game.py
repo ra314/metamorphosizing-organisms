b045053bd06bedd259792cb0b0285825cd3b1d04
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-04-17 17:43:12 +1000

    Added select_first_player. Added some variables to player.

--- Game.py
+++ Game.py
@@ -10,10 +10,17 @@ class Game:
 	def randomise_arena(self):
 		arena = random.choice(['stadium', 'forest valley', 'abandoned town'])
 		if arena == 'stadium':
-			Player.max_HP = 60
+			player.max_HP = 60
+			player1.curr_HP = 60
+			player2.curr_HP = 60
 			
 		elif arena == 'forest valley':
 			Organism.berries_to_evolve = 3
 			
 		elif arena == 'abandoned town':
 			Organism.HP_restored_on_evolution = 10
+			
+	def select_first_player(self):
+		first_player = random.choice([self.player1, self.player2])
+		first_player.myTurn = True
+		first_player.curr_HP += 5
--- Player.py
+++ Player.py
@@ -1,12 +1,15 @@
 class Player:
 	max_HP = 80
+	moves_per_turn = 2
 	
 	def __init__(self, organism1, organism2, name):
 		self.organism1 = organism1
 		self.organism2 = organism2
 		self.num_berries = None
 		self.name = name
-		self.curr_hp = self.max_HP
+		self.curr_HP = self.max_HP
+		self.myTurn = False
+		self.moves = moves_per_turn
 		
 	def __str__(self):
 		return f"{self.name} {str(self.organism1)} {str(self.organism2)}"
