3ac6083fcf7ba8bb80f5e9491d0232e2900e6712
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-04-17 12:44:00 +1000

    Improved readability in server by indenting ability creation
    Changed it so client picks index of organism

--- Server.py
+++ Server.py
@@ -2,18 +2,24 @@ from Organism import Organism
 from Ability import Ability
 
 def initialize_organisms():
-	Flare_plus = Ability('Flare+: Attacks for 25 HP and matches 2 random columns.', (0,5), ((1,-1),(1,-1)), 0, False, None, None, 8)
-	Flare = Ability('Flare: Attacks for 20 HP and matches a random column.', (0,20), ((1,-1)), 0, False, None, None, 8)
+	Flare_plus = Ability('Flare+: Attacks for 25 HP and matches 2 random columns.', 
+		(0,5), ((1,-1),(1,-1)), 0, False, None, None, 8)
+	Flare = Ability('Flare: Attacks for 20 HP and matches a random column.', 
+		(0,20), ((1,-1)), 0, False, None, None, 8)
 	Bonzire = Organism('Bonzumi', Flare_plus, 'fire', None)
 	Bonzumi = Organism('Bonzumi', Flare, 'fire', Bonzire)
 
-	Hydro_Rush_plus = Ability('Hydro Rush+: Attacks for 20 HP and converts 23random tiles to Water.', (0,20), None, 3, False, None, None, 6)
-	Hydro_Rush = Ability('Hydro Rush: Attacks for 10 HP and converts 2 random tiles to Water.', (0,10), None, 2, False, None, None, 6)
+	Hydro_Rush_plus = Ability('Hydro Rush+: Attacks for 20 HP and converts 23random tiles to Water.', 
+		(0,20), None, 3, False, None, None, 6)
+	Hydro_Rush = Ability('Hydro Rush: Attacks for 10 HP and converts 2 random tiles to Water.', 
+		(0,10), None, 2, False, None, None, 6)
 	Sephanix = Organism('Sephanix', Hydro_Rush_plus, 'water', None)
 	Pelijet = Organism('Pelijet', Hydro_Rush, 'water', Sephanix)
 
-	Heal_Leaf_plus = Ability('Heal Leaf+: Attacks for 15HP and heals you for 15HP.', (10,10), None, 0, False, None, None, 6)
-	Heal_Leaf = Ability('Heal Leaf: Attacks for 10HP and heals you for 10HP.', (10,10), None, 0, False, None, None, 6)
+	Heal_Leaf_plus = Ability('Heal Leaf+: Attacks for 15HP and heals you for 15HP.', 
+		(10,10), None, 0, False, None, None, 6)
+	Heal_Leaf = Ability('Heal Leaf: Attacks for 10HP and heals you for 10HP.', 
+		(10,10), None, 0, False, None, None, 6)
 	Karaggon = Organism('Karaggon', Heal_Leaf_plus, 'grass', None)
 	Turtleisk = Organism('Turtleisk', Heal_Leaf, 'grass', Karaggon)
 	
@@ -21,7 +27,8 @@ def initialize_organisms():
 	
 mana_types = ('fire', 'electric', 'water', 'grass')
 stage_1_organisms = initialize_organisms()
-organism_choices = '\n'.join([str(organism) for organism in stage_1_organisms])
+organism_choices = '\n'.join([f"{index} {str(organism)}" 
+	for index, organism in enumerate(stage_1_organisms)])
 		
 def create_player(client):
 	client.send("Send name: ".encode())
