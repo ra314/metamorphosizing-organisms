663dc2657d181eda1a088b161d0c4578a9a9f450
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-04-17 12:12:04 +1000

    Added file that prepares socket and also ensures that it closes.

---
+++ prepare_socket.py
@@ -0,0 +1,14 @@
+import socket
+T_PORT = 12345
+TCP_IP = '127.0.0.1'
+BUF_SIZE = 4096
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Ensuring the socket is closed
+import atexit
+def close_socket():
+	print("closed")
+	sock.close()
+
+atexit.register(close_socket)
