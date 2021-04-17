9676a600736276f19d7b349b8c9d31bc622e5488
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-04-17 18:46:39 +1000

    Added function find_grid_matches.

--- Game.py
+++ Game.py
@@ -5,11 +5,13 @@ import numpy as np
 from ManaType import mana_indexes 
 
 class Game:
-	grid_size = (7,5)
+	#Height and width
+	grid_size = (5,7)
 	
 	def __init__(self, player1, player2):
 		self.player1 = player1
 		self.player2 = player2
+		self.grid = None
 	
 	def randomise_arena(self):
 		arena = random.choice(['stadium', 'forest valley', 'abandoned town'])
@@ -30,4 +32,40 @@ class Game:
 		first_player.curr_HP += 5
 		
 	def generate_tiles(self):
-		grid = np.random.randint(0, len(mana_indexes), *grid_size)
+		self.grid = np.random.randint(0, len(mana_indexes), self.grid_size)
+		
+	def find_grid_matches(self):
+		matches = np.zeros(self.grid_size)
+		
+		def match_tile(y,x):
+			curr_tile = self.grid[y,x]
+			
+			#Check for matches above the curr tile
+			if y-2 >= 0:
+				if self.grid[y-1][x] == curr_tile and \
+					self.grid[y-2][x] == curr_tile:
+					matches[y,x] = 1
+					matches[y-1,x] = 1
+					matches[y-2,x] = 1
+			
+			#Check for matches to the left of the curr tile
+			if x-2 >= 0:
+				if self.grid[y][x-1] == curr_tile and \
+					self.grid[y][x-2] == curr_tile:
+					matches[y,x] = 1
+					matches[y,x-1] = 1
+					matches[y,x-2] = 1
+		
+		#Check for matches at each tile		
+		for y in range(self.grid_size[0]):
+			for x in range(self.grid_size[1]):
+				match_tile(y,x)
+		
+		#Count the number of matches for each tile type
+		matches_per_type = np.zeros(len(mana_indexes))
+		for y in range(self.grid_size[0]):
+			for x in range(self.grid_size[1]):
+				if matches[y,x]:
+					matches_per_type[grid[y,x]] += 1
+			
+		return matches
--- Player.py
+++ Player.py
@@ -9,7 +9,7 @@ class Player:
 		self.name = name
 		self.curr_HP = self.max_HP
 		self.myTurn = False
-		self.moves = moves_per_turn
+		self.moves = self.moves_per_turn
 		
 	def __str__(self):
 		return f"{self.name} {str(self.organism1)} {str(self.organism2)}"
