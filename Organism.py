59b0cb2668e744d8c6cae9d60ebef4588fe862b2
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-04-18 14:19:11 +1000

    Refactored Organism and Player in pycharm. Player now displays HP.

--- Organism.py
+++ Organism.py
@@ -1,26 +1,27 @@
 class Organism:
-	def __init__(self, name, ability, mana_type, evolution):
-		self.name = name
-		self.ability = ability
-		self.mana_type = mana_type
-		self.num_mana = 0
-		self.evolution = evolution
-		
-	def __str__(self):
-		return self.name
-		
-	def display(self):
-		mana_str = ('+' * self.num_mana) + ('-' * (self.ability.num_mana_to_activate - self.num_mana))
-		return self.name + '\n'\
-			+ mana_str
-		
-	def evolve(self):
-		self.name = self.evolution.name
-		self.ability = self.evolution.ability
-		self.evolution = None
-		
-	def change_num_mana(self, delta):
-		self.num_mana = (delta + self.num_mana) % self.ability.num_mana_to_activate
+    def __init__(self, name, ability, mana_type, evolution):
+        self.name = name
+        self.ability = ability
+        self.mana_type = mana_type
+        self.num_mana = 0
+        self.evolution = evolution
+
+    def __str__(self):
+        return self.name
+
+    def display(self):
+        mana_str = ('+' * self.num_mana) + ('-' * (self.ability.num_mana_to_activate - self.num_mana))
+        return f"{self.name} \n " \
+               f"mana: {mana_str}"
+
+    def evolve(self):
+        self.name = self.evolution.name
+        self.ability = self.evolution.ability
+        self.evolution = None
+
+    def change_num_mana(self, delta):
+        self.num_mana = (delta + self.num_mana) % self.ability.num_mana_to_activate
+
 
 from ManaType import mana_indexes
 from Ability import abilities
@@ -28,17 +29,17 @@ from Ability import abilities
 stage_2_organisms = {}
 stage_1_organisms = {}
 
-stage_2_organisms['Bonzire'] = Organism('Bonzumi', abilities['Flare_plus'], 
-	mana_indexes['fire'], None)
-stage_1_organisms['Bonzumi'] = Organism('Bonzumi', abilities['Flare'], 
-	mana_indexes['fire'], stage_2_organisms['Bonzire'])
+stage_2_organisms['Bonzire'] = Organism('Bonzumi', abilities['Flare_plus'],
+                                        mana_indexes['fire'], None)
+stage_1_organisms['Bonzumi'] = Organism('Bonzumi', abilities['Flare'],
+                                        mana_indexes['fire'], stage_2_organisms['Bonzire'])
 
-stage_2_organisms['Sephanix'] = Organism('Sephanix', abilities['Hydro_Rush_plus'], 		
-	mana_indexes['water'], None)
-stage_1_organisms['Pelijet'] = Organism('Pelijet', abilities['Hydro_Rush'], 
-	mana_indexes['water'], stage_2_organisms['Sephanix'])
+stage_2_organisms['Sephanix'] = Organism('Sephanix', abilities['Hydro_Rush_plus'],
+                                         mana_indexes['water'], None)
+stage_1_organisms['Pelijet'] = Organism('Pelijet', abilities['Hydro_Rush'],
+                                        mana_indexes['water'], stage_2_organisms['Sephanix'])
 
-stage_2_organisms['Karaggon'] = Organism('Karaggon', abilities['Heal_Leaf_plus'], 
-	mana_indexes['grass'], None)
-stage_1_organisms['Turtleisk'] = Organism('Turtleisk', abilities['Heal_Leaf'], 
-	mana_indexes['grass'], stage_2_organisms['Karaggon'])
+stage_2_organisms['Karaggon'] = Organism('Karaggon', abilities['Heal_Leaf_plus'],
+                                         mana_indexes['grass'], None)
+stage_1_organisms['Turtleisk'] = Organism('Turtleisk', abilities['Heal_Leaf'],
+                                          mana_indexes['grass'], stage_2_organisms['Karaggon'])
--- Player.py
+++ Player.py
@@ -1,43 +1,45 @@
 class Player:
-	max_HP = 80
-	moves_per_turn = 2
-	berries_to_evolve = 4
-	HP_restored_on_evolution = 0
-	
-	def __init__(self, organism1, organism2, name):
-		self.organisms = (organism1, organism2)
-		self.num_berries = None
-		self.name = name
-		self.curr_HP = self.max_HP
-		self.moves = self.moves_per_turn
-		
-	def __str__(self):
-		return self.name
-		
-	def display(self):
-		return self.name + '\n'\
-			+ self.organisms[0].display() + '\n'\
-			+ self.organisms[1].display()
-			
-	def get_actions(self):
-		actions = ["Swap 2 tiles"]
-		if self.num_berries == self.berries_to_evolve:
-			for organism in self.organisms:
-				if organism.evolution:
-					actions.append(f"Evolve {organism.name}")
-				else:
-					actions.append(f"Boost {organism.name}")
-		return actions
-		
-	def change_HP(self, delta):
-		self.curr_HP = (delta + self.curr_HP) % self.max_HP
-		
-	def change_num_berries(self, delta):
-		self.num_berries = (delta + self.num_berries) % self.berries_to_evolve
-		
-	def evolve_organism(self, organism_index):
-		self.change_HP(self.HP_restored_on_evolution)
-		self.organisms[organism_index].evolve()
-		
-	def boost_organism(self, organism_index):
-		self.organisms[organism_index].change_num_mana(self.num_berries)
+    max_HP = 80
+    moves_per_turn = 2
+    berries_to_evolve = 4
+    HP_restored_on_evolution = 0
+
+    def __init__(self, organism1, organism2, name):
+        self.organisms = (organism1, organism2)
+        self.num_berries = None
+        self.name = name
+        self.curr_HP = self.max_HP
+        self.moves = self.moves_per_turn
+
+    def __str__(self):
+        return self.name
+
+    def display(self):
+        HP_str = ('+' * self.curr_HP) + ('-' * (self.max_HP - self.curr_HP))
+        return f"{self.name} \n" \
+               f"HP: {HP_str} \n" \
+               f"{self.organisms[0].display()} \n" \
+               f"{self.organisms[1].display()}"
+
+    def get_actions(self):
+        actions = ["Swap 2 tiles"]
+        if self.num_berries == self.berries_to_evolve:
+            for organism in self.organisms:
+                if organism.evolution:
+                    actions.append(f"Evolve {organism.name}")
+                else:
+                    actions.append(f"Boost {organism.name}")
+        return actions
+
+    def change_HP(self, delta):
+        self.curr_HP = (delta + self.curr_HP) % self.max_HP
+
+    def change_num_berries(self, delta):
+        self.num_berries = (delta + self.num_berries) % self.berries_to_evolve
+
+    def evolve_organism(self, organism_index):
+        self.change_HP(self.HP_restored_on_evolution)
+        self.organisms[organism_index].evolve()
+
+    def boost_organism(self, organism_index):
+        self.organisms[organism_index].change_num_mana(self.num_berries)
